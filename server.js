e2b2443faf3660c267a2b1f0492e1442e9be0cdb
Oren <xxorenxx@gmail.com>
2025-01-27 15:48:20 +0200

    sent first bugs

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 import express from 'express'
+import { bugs } from './demo-data-bug.js'
+
 
 const app = express()
 
 app.get('/', (req, res) => res.send('Hello there'))
+
 app.listen(3030, () => console.log('Server ready at port 3030'))
+
+app.get('/api/bug', (req, res) => {
+    console.log('Sending bugs list...')
+    res.send(bugs)
+        .then((bugs) => console.log(bugs))
+        .catch((err) => console.log('error on getting bugs list'))
+})
+
+app.get('/api/bug/save', (req, res) => {})
+
+app.get('/api/bug/:bugId', (req, res) => {})
+
+app.get('/api/bug/:bugId/remove', (req, res) => {})
